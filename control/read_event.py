0487ac731538ab25d446d08625a5570658c14ba2
rogerramosruiz <rogerramosruizdev@gmail.com>
2024-06-01 23:06:08 -0400

    get touchscreen event

---
+++ control/read_event.py
@@ -0,0 +1,22 @@
+import subprocess
+
+def get_event_name(getevent_output:str):
+    lines = getevent_output.splitlines()
+    current_event = ''
+    for line in lines:
+        if 'add device' in line:
+            current_event = line.split(':')[-1].split('/')[-1]
+        if 'INPUT_PROP_DIRECT' in line:
+            return current_event
+    return None
+
+def get_event():
+    getevent_output = subprocess.check_output(['getevent', '-il']).decode("utf-8")
+    event_name = get_event_name(getevent_output)
+    if event_name:
+        with open('event.txt', 'w') as f:
+            f.write(event_name)
+    else:
+        raise Exception("Couldn't get event for touchscreen try running as a super user")
+if __name__ == '__main__':
+    get_event()
