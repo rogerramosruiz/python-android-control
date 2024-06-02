f6ad6a7b6b6299a59f46e55dda6928e269b166fd
rogerramosruiz <rogerramosruizdev@gmail.com>
2024-06-02 11:00:49 -0400

    refactor event execution

--- control/screen.py
+++ control/screen.py
@@ -1,19 +1,21 @@
 import subprocess
 from . import event_name
 
+def execute_events(events: list):
+    for event in events:
+       subprocess.run(["sendevent", f"/dev/input/{event_name}"] + event)
+
 def press(x:float, y:float):
     """
     Press on the screen on coordenates x,y
     """
-    events = [["sendevent", f"/dev/input/{event_name}", "3", "57", "211"],
-            ["sendevent", f"/dev/input/{event_name}", "3", "53", f"{x}"],
-            ["sendevent", f"/dev/input/{event_name}", "3", "54", f"{y}"],
-            ["sendevent", f"/dev/input/{event_name}", "1", "330", "1"],
-            ["sendevent", f"/dev/input/{event_name}", "0", "0", "0"],
-            ["sendevent", f"/dev/input/{event_name}", "3", "57", "4294967295"],
-            ["sendevent", f"/dev/input/{event_name}", "1", "330", "0"],
-            ["sendevent", f"/dev/input/{event_name}", "0", "0", "0"]
-    ]
-
-    for event in events:
-       subprocess.run(event)
+    events = [["3", "57", "211"],
+            ["3", "53", f"{x}"],
+            ["3", "54", f"{y}"],
+            ["1", "330", "1"],
+            ["0", "0", "0"],
+            ["3", "57", "4294967295"],
+            ["1", "330", "0"],
+            ["0", "0", "0"]
+        ]
+    execute_events(events=events)
